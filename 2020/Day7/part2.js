269679ed9d9e9d9046a470cc072942c67dd72945
Sjoerd <sjoerdvbavel@gmail.com>
2020-12-07 19:02:10 +0100

    Added day 7 (part 2 incomplete)

---
+++ 2020/Day7/part2.js
@@ -0,0 +1,42 @@
+var fs = require('fs');
+var path = require('path');
+var filePath = path.join(__dirname, 'testdata.txt');
+var array = fs.readFileSync(filePath).toString().split("\n");
+
+function includesButNotAtTheStart(wordToCompare) {
+    return function(element) {
+        return element.indexOf(wordToCompare) > 0;
+    }
+}
+
+function getBagName(string){
+    let bagname = string.split(' contain')[0];
+    return bagname.substring(0, bagname.length-1);
+}
+
+function FindAllBagsContaining(string){
+    var BagsContaining = array.filter(includesButNotAtTheStart(string));
+
+    return BagsContaining.map(n=>getBagName(n));
+}
+function FindAllBagsRecursive(bagname){
+    var bags2Search = FindAllBagsContaining(bagname),
+        bags2return = bags2Search.slice();
+    
+    console.log(bags2return);
+    while(bags2Search.length){
+        //console.log(bags2Search);
+        var searchon = bags2Search.shift();
+        var newbags = FindAllBagsContaining(searchon);
+        let difference = newbags.filter(x => !bags2return.includes(x));
+
+        var bags2Search = bags2Search.concat(difference);
+        var bags2return = bags2return.concat(difference);
+
+    }
+    return(bags2return);
+}
+
+var output = FindAllBagsRecursive("shiny gold bag");
+console.log(output);
+console.log("Found " + output.length + " bags")
