1bec720dfbd4de695fe74e8673e0007a40f970e3
Sjoerd <sjoerdvbavel@gmail.com>
2020-12-07 22:35:49 +0100

    Added part2 of day 7

--- 2020/Day7/part2.js
+++ 2020/Day7/part2.js
@@ -1,42 +1,36 @@
 var fs = require('fs');
 var path = require('path');
-var filePath = path.join(__dirname, 'testdata.txt');
+var filePath = path.join(__dirname, 'data.txt');
 var array = fs.readFileSync(filePath).toString().split("\n");
 
-function includesButNotAtTheStart(wordToCompare) {
-    return function(element) {
-        return element.indexOf(wordToCompare) > 0;
+function getBagNames(string) {
+    return bagnames = string.match(/\d\s\w+\s\w+\sbag/g);
+}
+
+function FindBagline(string) {
+    return BagsContaining = array.find(n => n.indexOf(string) == 0);
+}
+
+function FindAllChildrenRecursive(bagname) {
+    var bagline = FindBagline(bagname)
+
+    bagchildren = getBagNames(bagline);
+
+    var count = 1;
+    if (!bagchildren) {
+        return count;
+    } else {
+        for (child of bagchildren) {
+            var number = child.substring(0, 1)
+            var bagname = child.substring(2, child.length);
+            var bagsperchild = FindAllChildrenRecursive(bagname);
+            count += number * bagsperchild;
+            //console.log(bagname + " totaling " + count +" bags");
+        }
     }
+    return count;
 }
 
-function getBagName(string){
-    let bagname = string.split(' contain')[0];
-    return bagname.substring(0, bagname.length-1);
-}
-
-function FindAllBagsContaining(string){
-    var BagsContaining = array.filter(includesButNotAtTheStart(string));
-
-    return BagsContaining.map(n=>getBagName(n));
-}
-function FindAllBagsRecursive(bagname){
-    var bags2Search = FindAllBagsContaining(bagname),
-        bags2return = bags2Search.slice();
-    
-    console.log(bags2return);
-    while(bags2Search.length){
-        //console.log(bags2Search);
-        var searchon = bags2Search.shift();
-        var newbags = FindAllBagsContaining(searchon);
-        let difference = newbags.filter(x => !bags2return.includes(x));
-
-        var bags2Search = bags2Search.concat(difference);
-        var bags2return = bags2return.concat(difference);
-
-    }
-    return(bags2return);
-}
-
-var output = FindAllBagsRecursive("shiny gold bag");
-console.log(output);
-console.log("Found " + output.length + " bags")
+var target = "shiny gold bag";
+var output = FindAllChildrenRecursive(target)-1; //minus one because we count the gold bag which should not be counted.
+console.log(target + " contains " + output + " bags")
