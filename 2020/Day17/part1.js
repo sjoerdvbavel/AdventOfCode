33da2b66fb4c95e33a21748cc1af65388445e2ed
Sjoerd <sjoerdvbavel@gmail.com>
2021-12-05 14:42:41 +0100

    Added a finished day17

--- 2020/Day17/part1.js
+++ 2020/Day17/part1.js
@@ -1,104 +1,173 @@
 var fs = require('fs');
 var path = require('path');
 var filePath = path.join(__dirname, 'data.txt');
-var array = fs.readFileSync(filePath).toString().split("\r\n");
+var dataSet = fs.readFileSync(filePath).toString().split("\r\n");
 
-var completearray = array.map(str => str.split(''));
-var h = completearray.length;
-var w = completearray[0].length;
-console.log(h + " " + w);
+var startingField = dataSet.map(str => str.split(''));
 
-//Check whether a seat is occupied.
-function IsActiveOccupiedSeat(str) {
+var w = startingField[0].length;
+var h = startingField.length;
+
+//For this we create a field of the maximal size that can be reached. The starting field and a border of 6 around it.
+//Oriented as: field[z][y][x]
+//Origin of the starting field is: (n, n, n) to (n+w, n+h, n)
+
+let n = 6;
+let xdist = w + 2 * n;
+let ydist = h + 2 * n;
+let zdist = 1 + 2 * n;
+
+
+//Check whether a cell is occupied.
+function IsActive(str) {
     return str == '#';
 }
 
 
-//return how many occupied neighbours a given seat in a field has. 
 function GetActive(xvalue, yvalue, zvalue, cube) {
     // console.log("x "+ xvalue +" y " + yvalue);
-    let Neighbours = 0;
+    let neighbours = 0;
     let range = [-1, 0, 1];
-    for(xcursor of range){
-        for(ycursor of range){
-            for(zcursor of range){
-                if(xcursor != 0 or ycursor != 0 or zcursor != 0){
-                    if(IsActiveOccupiedSeatcube[xcursor][ycursor][zcursor])){
+    for (let zcursor of range) {
+        for (let ycursor of range) {
+            for (let xcursor of range) {
+                let x = xvalue + xcursor;
+                let y = yvalue + ycursor;
+                let z = zvalue + zcursor;
+                let fieldtoCheck = (xcursor != 0 || ycursor != 0 || zcursor != 0)
+                    && (x >= 0 && y >= 0 && z >= 0)
+                    && (x < xdist && y < ydist && z < zdist);
+                if (fieldtoCheck) {
+                    if (IsActive(cube[z][y][x])) {
                         neighbours++;
-                        if(neighbours > 3){
-                            return(4);
+                        if (neighbours > 3) {
+                            return (4);
                         }
-                    }   
+                    }
                 }
             }
-        }   
+        }
     }
-    return Neighbours;
+    return neighbours;
+}
+
+//Generate an empty field with size xdist,ydist,zdist.
+function NewField() {
+    let newfield = []
+    //Setup an empty field
+    for (let i = 0; i < zdist; i++) {
+        plate = []
+        for (let j = 0; j < ydist; j++) {
+            plate.push(new Array(xdist).fill('.'));
+        }
+        newfield.push(plate);
+    }
+    return newfield;
+}
+
+//Generate an empty field and enter the starting state.
+function GenerateField(startingfield) {
+    let newfield = NewField();
+
+    for (let k = 0; k < startingfield.length; k++) {
+        for (let l = 0; l < startingfield[0].length; l++) {
+            newfield[n][n + k][n + l] = startingfield[k][l];
+        }
+    }
+    return newfield;
 }
 
 //Console log the new field.
 function printField(field) {
-    for (line of field) {
-        console.log(line.join(''));
+    for (let k = 0; k < field.length; k++) {
+        if (JSON.stringify(field[k]).includes('#')) {
+            console.log('z = ' + (k));
+            console.log('  123456789012345');
+            for (let l = 0; l < field[0].length; l++) {
+                console.log((l % 10) + ' ' + field[k][l].join(''));
+            }
+        }
     }
 }
 
-//Create a new unrelated field
-function sliceField(field) {
-    let newfield = []
-    for (a in field) {
-        newfield[a] = field[a].slice();
+
+
+//Console log the counts of a field (capped at 4).
+function printCounts(field) {
+    let newfield = NewField();
+
+    for (var a = 0; a < xdist; a++) {
+        for (var b = 0; b < ydist; b++) {
+            for (var c = 0; c < zdist; c++) {
+                let numberactive = GetActive(a, b, c, field);
+                newfield[c][b][a] = numberactive.toString();
+
+            }
+        }
     }
-    console.assert(areFieldsEqual(field, newfield));
-    return newfield;
+    for (let k = 0; k < newfield.length; k++) {
+        if (JSON.stringify(newfield[k]).includes('1')) {
+            console.log('z = ' + (k));
+            console.log('  123456789012345');
+            for (let l = 0; l < newfield[0].length; l++) {
+                console.log((l % 10) + ' ' + newfield[k][l].join(''));
+            }
+        }
+    }
 }
+
 
 //generate a new iteration of the field.
 function GetNewField(field) {
-    var newfield = sliceField(field);
-    for (var a = 0; a < h; a++) {
-        for (var b = 0; b < w; b++) {
-            let neighbours = GetNeighbours(a, b, field);
-            if (neighbours == 0 && field[a][b] == 'L') {
-                newfield[a][b] = '#';
-            } else if (neighbours >= 4 && field[a][b] == '#') {
-                newfield[a][b] = 'L';
+    var newfield = NewField();
+    for (var a = 0; a < xdist; a++) {
+        for (var b = 0; b < ydist; b++) {
+            for (var c = 0; c < zdist; c++) {
+                let numberactive = GetActive(a, b, c, field);
+
+                if (numberactive == 3) {
+                    newfield[c][b][a] = '#';
+                } else if (numberactive == 2 && field[c][b][a] == '#') {
+                    newfield[c][b][a] = '#';
+                }
             }
         }
     }
     return newfield;
 }
 
-//check whether two fields are the same.
-function areFieldsEqual(field1, field2) {
-    return JSON.stringify(field1) === JSON.stringify(field2); //given by stackexchange, not very efficient.
-}
 
-function countOccupiedSeats(field) {
+function countLiveCells(field) {
     let count = 0;
     for (a in field) {
         for (b in field[a]) {
-            if (field[a][b] == '#') {
-                count++;
+            for (c in field[a][b]) {
+                if (field[a][b][c] == '#') {
+                    count++;
+                }
             }
         }
     }
     return count;
 }
 
-var field = completearray;
-console.log("Field");
+var field = GenerateField(dataSet);
 printField(field);
+// printCounts(field);
+// field = GetNewField(field);
+// printField(field);
 
-var newfield = GetNewField(field);
-
-
-
-while (!areFieldsEqual(field, newfield)) {
-    field = newfield;
-    newfield = GetNewField(field);
-    
+for(i = 0; i < 6; i++){
+field = GetNewField(field);
 }
-console.log("finalfield");
-printField(newfield)
-console.log("Occupied seats: " + countOccupiedSeats(newfield));
+console.log(countLiveCells(field));
+
+
+
+// while (!areFieldsEqual(field, newfield)) {
+//     field = newfield;
+//     newfield = GetNewField(field);
+// }
+// console.log("finalfield");
+// printField(newfield)
+// console.log("Occupied seats: " + countOccupiedSeats(newfield));
---
+++ 2020/Day17/part2.js
@@ -0,0 +1,191 @@
+var fs = require('fs');
+var path = require('path');
+var filePath = path.join(__dirname, 'data.txt');
+var dataSet = fs.readFileSync(filePath).toString().split("\r\n");
+
+var startingField = dataSet.map(str => str.split(''));
+
+var w = startingField[0].length;
+var h = startingField.length;
+
+//For this we create a field of the maximal size that can be reached. The starting field and a border of 6 around it.
+//Oriented as: field[w][z][y][x]
+//Origin of the starting field is: (n, n, n, n) to (n+w, n+h, n, n)
+
+let n = 6;
+let xdist = w + 2 * n;
+let ydist = h + 2 * n;
+let zdist = 1 + 2 * n;
+let wdist = 1 + 2 * n;
+
+//Generate an empty field with size xdist,ydist,zdist,wdist.
+function NewField() {
+    let newfield = []
+    //Setup an empty field
+    for (let i = 0; i < wdist; i++) {
+        cube = []
+        for (let j = 0; j < zdist; j++) {
+            plate = []
+            for (let k = 0; k < ydist; k++) {
+                plate.push(new Array(xdist).fill('.'));
+            }
+            cube.push(plate);
+        }
+        newfield.push(cube);
+    }
+    return newfield;
+}
+
+//Generate an empty field and enter the starting state.
+function GenerateField(startingfield) {
+    let newfield = NewField();
+
+    for (let k = 0; k < startingfield.length; k++) {
+        for (let l = 0; l < startingfield[0].length; l++) {
+            newfield[n][n][n + k][n + l] = startingfield[k][l];
+        }
+    }
+    return newfield;
+}
+
+//generate a new iteration of the field.
+function GetNewField(field) {
+    var newfield = NewField();
+    for (var a = 0; a < xdist; a++) {
+        for (var b = 0; b < ydist; b++) {
+            for (var c = 0; c < zdist; c++) {
+                for (var d = 0; d < wdist; d++) {
+                    let numberactive = GetActive(a, b, c, d, field);
+
+                    if (numberactive == 3) {
+                        newfield[d][c][b][a] = '#';
+                    } else if (numberactive == 2 && field[d][c][b][a] == '#') {
+                        newfield[d][c][b][a] = '#';
+                    }
+                }
+            }
+        }
+    }
+    return newfield;
+}
+
+//Check whether a cell is occupied.
+function IsActive(str) {
+    return str == '#';
+}
+
+
+function GetActive(xvalue, yvalue, zvalue, wvalue, cube) {
+    // console.log("x "+ xvalue +" y " + yvalue);
+    let neighbours = 0;
+    let range = [-1, 0, 1];
+    for (let wcursor of range) {
+        let w = wvalue + wcursor;
+        for (let zcursor of range) {
+            let z = zvalue + zcursor;
+            for (let ycursor of range) {
+                let y = yvalue + ycursor;
+                for (let xcursor of range) {
+                    let x = xvalue + xcursor;
+                    let fieldtoCheck = (xcursor != 0 || ycursor != 0 || zcursor != 0 || wcursor != 0)
+                        && (x >= 0 && y >= 0 && z >= 0 && w >= 0)
+                        && (x < xdist && y < ydist && z < zdist && w < wdist);
+                    if (fieldtoCheck) {
+                        if (IsActive(cube[w][z][y][x])) {
+                            neighbours++;
+                            if (neighbours > 3) {
+                                return (4);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return neighbours;
+}
+
+//Console log the new field.
+function printField(field) {
+    for (let k = 0; k < field.length; k++) {
+        if (JSON.stringify(field[k]).includes('#')) {
+            for (let l = 0; l < field[k].length; l++) {
+                if (JSON.stringify(field[k][l]).includes('#')) {
+                    console.log('z = ' + l + ', w=' + k);
+                    console.log('  123456789012345');
+                    for (let m = 0; m < field[0][0].length; m++) {
+                        console.log((m % 10) + ' ' + field[k][l][m].join(''));
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+//Console log the counts of a field (counts are capped at 4).
+function printCounts(field) {
+    let newfield = NewField();
+
+    for (var a = 0; a < xdist; a++) {
+        for (var b = 0; b < ydist; b++) {
+            for (var c = 0; c < zdist; c++) {
+                for (var d = 0; d < wdist; d++) {
+                    let numberactive = GetActive(a, b, c, d, field);
+                    newfield[d][c][b][a] = numberactive.toString();
+                }
+            }
+        }
+    }
+
+    for (let k = 0; k < newfield.length; k++) {
+        if (JSON.stringify(newfield[k]).includes('1')) {
+            for (let l = 0; l < newfield.length; l++) {
+                if (JSON.stringify(newfield[l]).includes('1')) {
+                    console.log('z = ' + l + ', w=' + k);
+                    console.log('  123456789012345');
+                    for (let m = 0; m < newfield[0].length; m++) {
+                        console.log((m % 10) + ' ' + newfield[k][l][m].join(''));
+                    }
+                }
+            }
+        }
+    }
+}
+
+function countLiveCells(field) {
+    let count = 0;
+    for (a in field) {
+        for (b in field[a]) {
+            for (c in field[a][b]) {
+                for (d in field[a][b][c]) {
+                    if (field[a][b][c][d] == '#') {
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+    return count;
+}
+
+var field = GenerateField(dataSet);
+printField(field);
+// printCounts(field);
+// field = GetNewField(field);
+// printField(field);
+
+for (i = 0; i < 6; i++) {
+    field = GetNewField(field);
+}
+console.log(countLiveCells(field));
+
+
+
+// while (!areFieldsEqual(field, newfield)) {
+//     field = newfield;
+//     newfield = GetNewField(field);
+// }
+// console.log("finalfield");
+// printField(newfield)
+// console.log("Occupied seats: " + countOccupiedSeats(newfield));
