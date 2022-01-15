c4eef6d9d7c3c2f961712ed0b40837a11898cfd3
Sjoerd <sjoerdvbavel@gmail.com>
2022-01-15 12:33:43 +0100

    Last changes day24

--- 2021/Day24/part2.js
+++ 2021/Day24/part2.js
@@ -10,8 +10,6 @@ var dataset = fs.readFileSync(filePath).toString();
 //parse instructions
 var instructions = dataset.split('\r\n').map(x => x.split(' '));
 
-
-
 function executeInstruction(instruction, state, nextinput) {
     if (instruction[0] == 'inp') {
         state[instruction[1]] = nextinput;
@@ -70,72 +68,57 @@ function ExecuteProgram(program, initialstate, input) {
     return { error: false, state: state };
 }
 
-function testString(string){
-    console.log(JSON.stringify(ExecuteProgram(instructions, { w: 0, x: 0, y: 0, z: 0 }, "99996953827143".split('').map(x=>parseInt(x,10))).state));
-}
-
-function reduceArray(array) {
+function increaseArray(array) {
     if (array.length == 0) {
         return [0];
     }
     let lastnumber = array.length - 1;
-    if (array[lastnumber] != 1) {
-        array[lastnumber] = array[lastnumber] - 1;
+    if (array[lastnumber] != 9) {
+        array[lastnumber] = array[lastnumber] + 1;
         return array;
     } else {
-        return [...reduceArray(array.slice(0, lastnumber)), 9];
+        return [...increaseArray(array.slice(0, lastnumber)), 1];
     }
 }
 
-//Some tests
-console.log(JSON.stringify(ExecuteProgram([['inp', 'x'], ['mul', 'x', '-1']], { w: 0, x: 0, y: 0, z: 0 }, [1])));
-
 //Execute the program.
 
-//Start with a large number:
+//Start with the lowest possible 14 digit number with no 0's: 1....1
+var potentialnumber = [1].concat(new Array(14).fill(1));
 
-var potentialnumber = new Array(14).fill(1);
-let leadingdigit = 10;
-while (parseInt(potentialnumber.join(''), 10) > 10^13) {
-    var initialstate = { w: 0, x: 0, y: 0, z: 0 };
-    // result = ExecuteProgram(instructions, initialstate, potentialnumber);
+let leadingdigit = -1;
+while (potentialnumber[0] == 1) {//Because of part 1 I know i can stop at 2*10^14th.
     let xcounter = 0;
     let ztotal = 0;
     let survived = true;
     for (let i = 0; i < 14; i++) {
-        //Exe
         let currentsubprogram = instructions.slice(18 * (i), 18 * (i+1));
         result = ExecuteProgram(currentsubprogram, { w: 0, x: 0, y: 0, z: ztotal}, [potentialnumber[i]]);
         ztotal = result.state.z;
         xcounter += result.state.x;
-        if(result.state.x != 0 && result.state.x != 1){
-            // console.log(`Found x = ${result.state.x} number ${potentialnumber.join('')} at step ${i}.`);
-        }
-        if (xcounter > 8) {//I expect the sum of all x'es has to be 6 or less.
-            potentialnumber = reduceArray(potentialnumber.slice(0, i)).concat(new Array(14 - i).fill(9));
+        if (xcounter > 7) {//I expect the sum of all x'es has to be 7 or less.
+            potentialnumber = increaseArray(potentialnumber.slice(0, i+1)).concat(new Array(14 - (i+1)).fill(1));
             survived = false;
             break;
-            // console.log(`xcounter too high, continue with ${potentialnumber.join('')}.`);
         }
     }
     if(survived){
-        console.log(`Number ${potentialnumber.join('')} ${JSON.stringify(result.state)}, x ${xcounter}`);
-        // console.log(`${JSON.stringify(ExecuteProgram(instructions, { w: 0, x: 0, y: 0, z: 0}, potentialnumber).state)}`);
-        potentialnumber = reduceArray(potentialnumber);
-    } else {
-    }
-
+        console.log(`Number ${potentialnumber.join('')} ${JSON.stringify(result.state)}, xcounter: ${xcounter}`);
+        if (result.state.z == 0) {
+            console.log(`Found a valid number: ${potentialnumber.join('')} check: ${JSON.stringify(ExecuteProgram(instructions, { w: 0, x: 0, y: 0, z: 0 }, potentialnumber).state)}`);
+            break;
+        } else{
+        potentialnumber = increaseArray(potentialnumber);
+        }
+    }    
     if (result.error) {
         let errorspot = potentialnumber.slice(0, result.errorinput);
-        potentialnumber = reduceArray(errorspot).concat(new Array(14 - result.errorinput).fill(9));
+        potentialnumber = increaseArray(errorspot).concat(new Array(14 - result.errorinput).fill(1));
         console.log(`error occured at ${errorspot.join('')}, continue with ${potentialnumber.join('')}. Message: ${result.errormessage}`);
-    } else if (result.state.z == 0) {
-        console.log(`Found a valid number ${potentialnumber.join('')}. check: ${ExecuteProgram(instructions, { w: 0, x: 0, y: 0, z: 0 }, potentialnumber)}`);
-        break;
     } else {
-        if (potentialnumber[0] < leadingdigit) {
-            console.log(`Number ${potentialnumber.join('')} invalid, ztotal = ${ztotal}`);
-            leadingdigit = potentialnumber[0];
+        if (potentialnumber[1] > leadingdigit) {
+            console.log(`Currently at: ${potentialnumber.join('')}`);
+            leadingdigit = potentialnumber[1];
         }
     }
 }
