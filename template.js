8702765a2c8928f535346fbae47bec595f4cbe62
svb31 <sjoerd@svb31.nl>
2022-02-03 07:42:58 +0100

    Added a timer function to the template

--- template.js
+++ template.js
@@ -29,26 +29,37 @@ function executePart2(dataset) {
     return -1;
 }
 
-function execute() {
+function execute(){ 
+    const { performance } = require('perf_hooks');
+
     let testdata1 = parseData('testdata.txt');
+    var starttd1 = performance.now();
     let testresult1 = executePart1(testdata1);
+    var endtd1 = performance.now();
     if (testresult1) {
-        console.log(`testdata part1: ${testresult1}`);
+        console.log(`testdata part1: ${testresult1} (${Math.round(endtd1 - starttd1)} ms)`);
     }
     let testdata2 = parseData('testdata.txt');
+    var starttd2 = performance.now();
     let testresult2 = executePart2(testdata2);
+    var endtd2 = performance.now();
     if (testresult2) {
-        console.log(`testdata part2: ${testresult2}`);
+        console.log(`testdata part2: ${testresult2} (${Math.round(endtd2 - starttd2)} ms)`);
     }
     let realdata1 = parseData('data.txt');
+
+    var startd1 = performance.now();
     let result1 = executePart1(realdata1);
+    var endd1 = performance.now();
     if (result1) {
-        console.log(`part1: ${result1}`);
+        console.log(`part1: ${result1} (${Math.round(endd1 - startd1)} ms)`);
     }
     let realdata2 = parseData('data.txt');
+    var startd2 = performance.now();
     let result2 = executePart2(realdata2);
+    var endd2 = performance.now();
     if (testresult2) {
-        console.log(`part2: ${result2}`);
+        console.log(`part2: ${result2} (${Math.round(endd2 - startd2)} ms)`);
     }
 }
 
