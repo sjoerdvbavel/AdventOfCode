56e24a9e5a4209e84140a48efabfea61e811349e
svb31 <sjoerd@svb31.nl>
2022-02-15 18:39:46 +0100

    Small tweaks 2 template

--- template.js
+++ template.js
@@ -39,6 +39,7 @@ function execute(){
     if (testresult1) {
         console.log(`testdata part1: ${testresult1} (${Math.round(endtd1 - starttd1)} ms)`);
     }
+    
     let testdata2 = parseData('testdata.txt');
     var starttd2 = performance.now();
     let testresult2 = executePart2(testdata2);
@@ -46,19 +47,20 @@ function execute(){
     if (testresult2) {
         console.log(`testdata part2: ${testresult2} (${Math.round(endtd2 - starttd2)} ms)`);
     }
-    let realdata1 = parseData('data.txt');
 
+    let realdata1 = parseData('data.txt');
     var startd1 = performance.now();
     let result1 = executePart1(realdata1);
     var endd1 = performance.now();
     if (result1) {
         console.log(`part1: ${result1} (${Math.round(endd1 - startd1)} ms)`);
     }
+
     let realdata2 = parseData('data.txt');
     var startd2 = performance.now();
     let result2 = executePart2(realdata2);
     var endd2 = performance.now();
-    if (testresult2) {
+    if (result2) {
         console.log(`part2: ${result2} (${Math.round(endd2 - startd2)} ms)`);
     }
 }
