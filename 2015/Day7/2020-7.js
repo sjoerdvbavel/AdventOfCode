d6b95a2585f14d0e86dbf8ae735c5906219a2592
Sjoerd <sjoerdvbavel@gmail.com>
2022-02-22 20:26:07 +0100

    Added 2020 7

---
+++ 2015/Day7/2020-7.js
@@ -0,0 +1,49 @@
+function parseData(filename) {
+    var fs = require('fs');
+    var path = require('path');
+    var filePath = path.join(__dirname, filename);
+    var rawDataSet = fs.readFileSync(filePath).toString().split("\r\n");
+
+    let dataset = [];
+    for (line of rawDataSet) {
+        splits = line.split(' -> ');
+    dataset.push({ });
+    }
+
+    return dataset;
+}
+
+function executePart1(dataset) {
+ 
+    return -1;
+}
+
+function executePart2(dataset) {
+
+    return -1;
+}
+
+function execute() {
+    let testdata1 = parseData('testdata.txt');
+    let testresult1 = executePart1(testdata1);
+    if (testresult1) {
+        console.log(`testdata part1: ${testresult1}`);
+    }
+    let testdata2 = parseData('testdata.txt');
+    let testresult2 = executePart2(testdata2);
+    if (testresult2) {
+        console.log(`testdata part2: ${testresult2}`);
+    }
+    let realdata1 = parseData('data.txt');
+    let result1 = executePart1(realdata1);
+    if (result1) {
+        console.log(`part1: ${result1}`);
+    }
+    let realdata2 = parseData('data.txt');
+    let result2 = executePart2(realdata2);
+    if (testresult2) {
+        console.log(`part2: ${result2}`);
+    }
+}
+
+execute();
