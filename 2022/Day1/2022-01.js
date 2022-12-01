189807921c353aca607a335cac71c056a2a940a2
Sjoerd <sjoerdvbavel@gmail.com>
2022-12-01 06:29:31 +0100

    Added day1 2022

---
+++ 2022/Day1/2022-01.js
@@ -0,0 +1,68 @@
+function unitTest(array, stringvalue) {
+    if (JSON.stringify(array) != stringvalue) {
+        console.log(`Test failed ${JSON.stringify(array)} != ${stringvalue}`);
+    }
+}
+
+function parseData(filename) {
+    var fs = require('fs');
+    var path = require('path');
+    var filePath = path.join(__dirname, filename);
+    var rawDataSet = fs.readFileSync(filePath).toString().split("\r\n\r\n");
+
+    let dataset = [];
+    for (block of rawDataSet) {
+         dataset.push(block.split("\r\n").map(a => Number(a)));
+    }
+
+    return dataset;
+}
+
+function executePart1(dataset) {
+    
+    return Math.max(...dataset.map(a => a.reduce((a,b)=> a+b, 0)));
+}
+
+function executePart2(dataset) {
+    let elvesTotal = dataset.map(a => a.reduce((a,b)=> a+b, 0)).sort((a,b)=>b-a).slice(0,3);
+    console.log(elvesTotal);
+    return elvesTotal.reduce((a,b)=> a+b, 0);
+}
+
+function execute(){ 
+    const { performance } = require('perf_hooks');
+
+    let testdata1 = parseData('testdata.txt');
+    var starttd1 = performance.now();
+    let testresult1 = executePart1(testdata1);
+    var endtd1 = performance.now();
+    if (testresult1) {
+        console.log(`testdata part1: ${testresult1} (${Math.round(endtd1 - starttd1)} ms)`);
+    }
+    
+    let testdata2 = parseData('testdata.txt');
+    var starttd2 = performance.now();
+    let testresult2 = executePart2(testdata2);
+    var endtd2 = performance.now();
+    if (testresult2) {
+        console.log(`testdata part2: ${testresult2} (${Math.round(endtd2 - starttd2)} ms)`);
+    }
+
+    let realdata1 = parseData('data.txt');
+    var startd1 = performance.now();
+    let result1 = executePart1(realdata1);
+    var endd1 = performance.now();
+    if (result1) {
+        console.log(`part1: ${result1} (${Math.round(endd1 - startd1)} ms)`);
+    }
+
+    let realdata2 = parseData('data.txt');
+    var startd2 = performance.now();
+    let result2 = executePart2(realdata2);
+    var endd2 = performance.now();
+    if (result2) {
+        console.log(`part2: ${result2} (${Math.round(endd2 - startd2)} ms)`);
+    }
+}
+
+execute();
